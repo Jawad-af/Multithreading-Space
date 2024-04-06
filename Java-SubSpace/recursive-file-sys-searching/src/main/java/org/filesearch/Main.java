16432ec050aeb549bdc4a31a8ac04ae69b0378af
Jawad-af <aboufakherj@gmail.com>
2024-04-07 00:05:53 +0300

    commiting the implementation of the algorithm
    no errors have occured

---
+++ Java-SubSpace/recursive-file-sys-searching/src/main/java/org/filesearch/Main.java
@@ -0,0 +1,31 @@
+package org.filesearch;
+
+import java.io.File;
+
+public class Main {
+
+    // TO BE TESTED AND COMPARED TOGETHER
+    public static void main(String[] args) throws InterruptedException {
+        SearchFileOneThread oneThread = new SearchFileOneThread();
+        SearchFileMultipleThreads multipleThreads = new SearchFileMultipleThreads();
+
+        String fileName = "2. File Search Example.mp4"; // PUT THE NAME OF THE FILE YOU WANT TO SEARCH FOR
+
+        var startSingle = System.currentTimeMillis();
+        oneThread.find(new File("D:/"), fileName);
+        var endSingle = System.currentTimeMillis();
+
+        var startMultiple = System.currentTimeMillis();
+        multipleThreads.find(new File("D:/"), fileName);
+        var endMultiple = System.currentTimeMillis();
+
+        System.out.println();
+        multipleThreads.matches.forEach((f) -> System.out.println(f));
+        System.out.println();
+        oneThread.matches.forEach((f) -> System.out.println(f));
+
+        System.out.println("\nThe evaluation of single thread took : " + (endSingle - startSingle)/1000.0 + " seconds");
+        System.out.println("The evaluation of multiple threads took : " + (endMultiple - startMultiple)/1000.0 + " seconds");
+
+    }
+}
---
+++ Java-SubSpace/recursive-file-sys-searching/src/main/java/org/filesearch/SearchFileMultipleThreads.java
@@ -0,0 +1,57 @@
+package org.filesearch;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SearchFileMultipleThreads {
+    static List<File> matches = new ArrayList<>();
+    static Lock lock = new ReentrantLock();
+
+    public void find(File file, String fileName) throws InterruptedException {
+        System.out.println("Searching in " + file.getAbsolutePath());
+        List<Thread> threads = new ArrayList<>();
+        File[] files = file.listFiles();
+        if (files == null) {
+            return;
+        }
+        for (File f : files) {
+            lock.lock();
+            if (f.getName().contains(fileName)) {
+                matches.add(f);
+            }
+            lock.unlock();
+            if (f.isDirectory()) {
+                Thread t = new Thread(() -> {
+                    try {
+                        find(f, fileName);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                threads.add(t);
+                t.start();
+            }
+        }
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+
+    // TO BE TESTED ALONE
+    public static void main(String[] args) throws InterruptedException {
+        SearchFileMultipleThreads multipleThreads = new SearchFileMultipleThreads();
+
+        String fileName = "2. File Search Example.mp4";
+
+        var start = System.currentTimeMillis();
+        multipleThreads.find(new File("D:/IT/courses/java-courses/"), fileName);
+        var end = System.currentTimeMillis();
+
+        System.out.println();
+        matches.forEach((f) -> System.out.println(f));
+        System.out.println("The evaluation took : " + (end - start)/1000.0 + " seconds");
+    }
+}
